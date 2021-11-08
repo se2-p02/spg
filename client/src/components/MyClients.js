333d076f0e922f5f63ab32801f73a4ceadc56108
Fabiano Campion <fabianocampion97@gmail.com>
2021-11-08 18:39:01 +0100

    Added link to single client

--- client/src/components/MyClients.js
+++ client/src/components/MyClients.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Button, Col, ListGroup, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import './MyNavBar.css';
 import API from "./API";
 
@@ -46,7 +46,7 @@ function MyClients(props) {
                         {
                             clients.map(c => {
                                 return (
-                                    <ListGroup key={c.id} className="my-2 mx-5" horizontal>
+                                    <ListGroup as={Link} to={"/employee/clients/" + c.id} key={c.id} style={{ textDecoration: 'none' }} className="my-2 mx-5" horizontal>
                                         <ListGroup.Item variant="primary" className="d-flex w-100 justify-content-center">{c.id}</ListGroup.Item>
                                         <ListGroup.Item variant="primary" className="d-flex w-100 justify-content-center">{c.name}</ListGroup.Item>
                                         <ListGroup.Item variant="primary" className="d-flex w-100 justify-content-center">{c.surname}</ListGroup.Item>
