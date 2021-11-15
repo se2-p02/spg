9df54ddf1176e549622869de626982b3770b2c46
Fabiano Campion <fabianocampion97@gmail.com>
2021-11-16 00:05:12 +0100

    test app js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/test/i);
+  expect(element).toBeInTheDocument();
 });
