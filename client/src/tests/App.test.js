5d99922d815669ea9f4192d02d689c1d3cfe5a78
Mattia <s280529@studenti.polito.it>
2021-11-16 10:47:12 +0100

    fix

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -10,6 +10,19 @@ test('renders app homepage', () => {
 
 test('renders app form', () => {
   render(<MyForm />);
-  const element = screen.getAllByPlaceholderText("Email");
+  var element = screen.getByPlaceholderText("Email");
+  expect(element).toBeInTheDocument();
   
+  element = screen.getByPlaceholderText("Name");
+  expect(element).toBeInTheDocument();
+
+  element = screen.getByPlaceholderText("Surname");
+  expect(element).toBeInTheDocument();
+  
+  element = screen.getByPlaceholderText("Password");
+  expect(element).toBeInTheDocument();
+  
+  element = screen.getByPlaceholderText("Repeat Password");
+  expect(element).toBeInTheDocument();
+
 });
