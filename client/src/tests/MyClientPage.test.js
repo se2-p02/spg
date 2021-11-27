3b9d3c6f4cebd1eb202c56473becebcc10a75929
Pietro <s283421@studenti.polito.it>
2021-11-27 10:22:23 +0100

    try new sonarcloud file

---
+++ client/src/tests/MyClientPage.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import MyClientPage from '../components/MyClientPage'
+
+test('renders app form', () => {
+    render(<MyClientPage />);
+    var element = screen.getByText("Profile")
+    expect(element).toBeInTheDocument();
+    
+    var element = screen.getByText("Products")
+    expect(element).toBeInTheDocument();
+  
+    element = screen.getByText("My Orders");
+    expect(element).toBeInTheDocument();
+  
+  });
