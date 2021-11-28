e01830844d33c9ea1083816e778d659c916392c8
Niusha Samadifar <s289894@studenti.polito.it>
2021-11-28 14:08:03 +0100

    writing apitest for getWallet and updateBasket small changes in modal window

--- server/tests/apis.spec.js
+++ server/tests/apis.spec.js
@@ -1,9 +1,9 @@
-const { describe } = require('jest-circus');
-const request = require('supertest');
+const { describe } = require("jest-circus");
+const request = require("supertest");
 const app = require("../app");
 
-describe('Products test', () => {
-  it('tests GET /api/products', async () => {
+describe("Products test", () => {
+  it("tests GET /api/products", async () => {
     const response = await request(app).get("/api/products");
     response.body.forEach((product) => {
       expect(product).toMatchSnapshot({
@@ -13,38 +13,41 @@ describe('Products test', () => {
         unit: expect.any(String),
         farmer: expect.any(Number),
         farmerName: expect.any(String),
-        price: expect.any(Number)
+        price: expect.any(Number),
       });
     });
   });
 });
 
-describe('Clients test', () => {
-  it('tests GET /api/clients', async () => {
+describe("Clients test", () => {
+  it("tests GET /api/clients", async () => {
     const response = await request(app).get("/api/clients");
     response.body.forEach((product) => {
       expect(product).toMatchSnapshot({
         id: expect.any(Number),
         name: expect.any(String),
-        surname: expect.any(String)
+        surname: expect.any(String),
       });
     });
   });
 
-  it('tests GET /api/clients/:id', async () => {
+  it("tests GET /api/clients/:id", async () => {
     const response = await request(app).get("/api/clients/-1");
     expect(response.body).toMatchSnapshot({
       id: expect.any(Number),
       name: expect.any(String),
       surname: expect.any(String),
       wallet: expect.any(Number),
-      email: expect.any(String)
+      email: expect.any(String),
     });
   });
 
-  it('tests PUT /api/clients/:id/wallet', async () => {
+  it("tests PUT /api/clients/:id/wallet", async () => {
     const wallet = 0.0;
-    await request(app).put("/api/clients/-1/wallet").send({ wallet }).expect(200);
+    await request(app)
+      .put("/api/clients/-1/wallet")
+      .send({ wallet })
+      .expect(200);
   });
 });
 /*
@@ -89,44 +92,91 @@ describe('Orders test', () => {
   });
 });
 */
-describe('Users test', () => {
-  it('tests POST /api/addNewUser', async () => {
-    const response = await request(app).post("/api/addNewUser").send({ name: "Mario", surname: "Rossi", password: "password", email: "test_email@email.it", phoneNumber: "3333333333", city: "Torino", address: "Via X, 5", country: "Italy", role: "customer" }).expect(200);
-    console.log(response.body.id)
-    const response2 = await request(app).post("/api/addNewUser").send({ name: "Mario", surname: "Rossi", password: "password", email: "test_email@email.it", phoneNumber: "3333333333", city: "Torino", address: "Via X, 5", country: "Italy", role: "customer" }).expect(500);
-    const deleteRes = await request(app).delete("/api/deleteUser").send({ email: "test_email@email.it" }).expect(200);
+describe("Users test", () => {
+  it("tests POST /api/addNewUser", async () => {
+    const response = await request(app)
+      .post("/api/addNewUser")
+      .send({
+        name: "Mario",
+        surname: "Rossi",
+        password: "password",
+        email: "test_email@email.it",
+        phoneNumber: "3333333333",
+        city: "Torino",
+        address: "Via X, 5",
+        country: "Italy",
+        role: "customer",
+      })
+      .expect(200);
+    console.log(response.body.id);
+    const response2 = await request(app)
+      .post("/api/addNewUser")
+      .send({
+        name: "Mario",
+        surname: "Rossi",
+        password: "password",
+        email: "test_email@email.it",
+        phoneNumber: "3333333333",
+        city: "Torino",
+        address: "Via X, 5",
+        country: "Italy",
+        role: "customer",
+      })
+      .expect(500);
+    const deleteRes = await request(app)
+      .delete("/api/deleteUser")
+      .send({ email: "test_email@email.it" })
+      .expect(200);
   });
 
-  it('tests error POST /api/addNewUser', async () => {
-    const response = await request(app).post("/api/addNewUser").send({ name: "Mario", surname: "Rossi", email: "test_email@email.it", phoneNumber: "3333333333", city: "Torino", address: "Via X, 5", country: "Italy", role: "customer" }).expect(500);
+  it("tests error POST /api/addNewUser", async () => {
+    const response = await request(app)
+      .post("/api/addNewUser")
+      .send({
+        name: "Mario",
+        surname: "Rossi",
+        email: "test_email@email.it",
+        phoneNumber: "3333333333",
+        city: "Torino",
+        address: "Via X, 5",
+        country: "Italy",
+        role: "customer",
+      })
+      .expect(500);
   });
 });
 
-describe('Session test', () => {
-  it('tests delete /api/sessions/current', async () => {
-    const deleteRes = await request(app).delete("/api/sessions/current").expect(200);
+describe("Session test", () => {
+  it("tests delete /api/sessions/current", async () => {
+    const deleteRes = await request(app)
+      .delete("/api/sessions/current")
+      .expect(200);
   });
 
-  it('tests get /api/sessions/current', async () => {
-    const deleteRes = await request(app).get("/api/sessions/current").expect(401);
+  it("tests get /api/sessions/current", async () => {
+    const deleteRes = await request(app)
+      .get("/api/sessions/current")
+      .expect(401);
   });
-
 });
 
-describe('Next week test', () => {
-  beforeEach(()=>{
+describe("Next week test", () => {
+  beforeEach(() => {
     return request(app).delete("/api/sessions/current").expect(200);
-  })
-  it('tests get /api/nextProducts without performing the login', () => {
+  });
+  it("tests get /api/nextProducts without performing the login", () => {
     const res = request(app).get("/api/nextProducts").expect(500);
   });
 });
 
-describe('Next week test', () => {
+describe("Next week test", () => {
   beforeEach(() => {
-    return request(app).post("/api/sessions").send({ username: "gigi@libero.it", password: "cagliari" }).expect(200);
+    return request(app)
+      .post("/api/sessions")
+      .send({ username: "gigi@libero.it", password: "cagliari" })
+      .expect(200);
   });
-  it('tests get /api/nextProducts after the login as a customer', async () => {
+  it("tests get /api/nextProducts after the login as a customer", async () => {
     // login
     const res = await request(app).get("/api/nextProducts");
     res.body.forEach((product) => {
@@ -138,20 +188,23 @@ describe('Next week test', () => {
         farmer: expect.any(Number),
         price: expect.any(Number),
         availability: expect.any(String),
-        filter: expect.any(String)
+        filter: expect.any(String),
       });
     });
   });
-  afterEach(()=>{
+  afterEach(() => {
     return request(app).delete("/api/sessions/current").expect(200);
-  })
+  });
 });
 
-describe('Next week test farmer', () => {
+describe("Next week test farmer", () => {
   beforeEach(() => {
-    return request(app).post("/api/sessions").send({ username: "farmer@farmer.farmer", password: "farmer" }).expect(200);
+    return request(app)
+      .post("/api/sessions")
+      .send({ username: "farmer@farmer.farmer", password: "farmer" })
+      .expect(200);
   });
-  it('tests get /api/nextProducts after the login as a farmer', async () => {
+  it("tests get /api/nextProducts after the login as a farmer", async () => {
     const res = await request(app).get("/api/nextProducts");
     res.body.forEach((product) => {
       expect(product).toMatchSnapshot({
@@ -162,22 +215,61 @@ describe('Next week test farmer', () => {
         farmer: expect.any(Number),
         price: expect.any(Number),
         availability: expect.any(String),
-        filter: expect.any(String)
+        filter: expect.any(String),
       });
     });
   });
-  afterEach(()=>{
+  afterEach(() => {
     return request(app).delete("/api/sessions/current").expect(200);
-  })
+  });
 });
 
-describe('login test', () => {
-  it('tests post /api/sesion', async () => {
-    const response = await request(app).post("/api/sessions").send({ username: "gigi@libero.it", password: "cagliari" }).expect(200);
-    const deleteRes = await request(app).delete("/api/sessions/current").expect(200);
+describe("login test", () => {
+  it("tests post /api/sesion", async () => {
+    const response = await request(app)
+      .post("/api/sessions")
+      .send({ username: "gigi@libero.it", password: "cagliari" })
+      .expect(200);
+    const deleteRes = await request(app)
+      .delete("/api/sessions/current")
+      .expect(200);
   });
-  it('tests post /api/sesion error', async () => {
-    const response = await request(app).post("/api/sessions").send({ username: "gigi@libero.it", password: "cagl" }).expect(401);
+  it("tests post /api/sesion error", async () => {
+    const response = await request(app)
+      .post("/api/sessions")
+      .send({ username: "gigi@libero.it", password: "cagl" })
+      .expect(401);
+  });
+});
+
+describe("update basket test", () => {
+  it("tests PUT /api/clients/basket/:id", async () => {
+    await request(app)
+      .put("/api/clients/basket/2")
+      .send({
+        id: 4,
+        name: "Cheese",
+        quantity: 2,
+        unit: "kg",
+        filter: "Dairy",
+        farmer: 3,
+        farmerName: "TerraGrossa",
+        price: 10.3,
+        availability: "2021-11-29",
+      })
+      .expect(200);
+  });
+});
+
+describe("get wallet test", () => {
+  it("tests GET /api/wallet/:id", async () => {
+    const response = await request(app).get("/api/wallet/2");
+    response.body.forEach((wallet) => {
+      expect(wallet).toMatchSnapshot({
+        wallet: expect.any(Number),
+      });
+    });
   });
 
+ 
 });
