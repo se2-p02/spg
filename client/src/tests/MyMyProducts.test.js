d95420ff4f8d2388ace3cc087b5defd954b66561
mattiamorosi <66827085+mattiamorosi@users.noreply.github.com>
2022-01-06 19:38:21 +0100

    TD management

--- client/src/tests/MyMyProducts.test.js
+++ client/src/tests/MyMyProducts.test.js
@@ -6,6 +6,29 @@ import MyMyProducts from "../components/MyMyProducts";
 import moment from 'moment'
 
 describe('Test MyMyproducts', () => {
+    it("tests components", async () => {
+        render(<MyMyProducts
+            clock={moment('2021-11-27 9:55')}
+            setClock={jest.fn()}
+            user={{ id: 7, role: "farmer", username: "farmer@farmer.farmer" }}
+            cart={[]}
+            setCart={jest.fn()}
+            showCart={true} />);
+        
+        var elem = screen.getByText("Id");
+        expect(elem).toBeInTheDocument();
+        elem = screen.getByText("Name");
+        expect(elem).toBeInTheDocument();
+        elem = screen.getByText("Quantity");
+        expect(elem).toBeInTheDocument();
+        elem = screen.getByText("Price");
+        expect(elem).toBeInTheDocument();
+        elem = screen.getByText("Modify");
+        expect(elem).toBeInTheDocument();
+        elem = screen.getByText("Confirm");
+        expect(elem).toBeInTheDocument();
+
+    });
     it("tests error messages", async () => {
         render(<MyMyProducts
             clock={moment('2021-11-27 8:55')}
