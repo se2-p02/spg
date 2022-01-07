c9df335963b33733959200d5036b6ebf5b6a3a4f
mattiamorosi <66827085+mattiamorosi@users.noreply.github.com>
2022-01-07 15:26:29 +0100

    test to increase coverage mystat

--- client/src/tests/MyStatistics.test.js
+++ client/src/tests/MyStatistics.test.js
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import MyStatistics from '../components/MyStatistics'
 import React from 'react'
+import renderWithRouter from "./setupTestsRouter";
 
 
 describe('Test MyStatistics', () => {
   test('renders the page with the statistics', () => {
-    render(<MyStatistics/>);
+    render(<MyStatistics
+      clock={moment('2021-11-27 7:55')}
+      user={{ id: 7, role: "farmer", username: "farmer@farmer.farmer" }}/>);
 
     var element = screen.getByText("Week");
     expect(element).toBeInTheDocument();
